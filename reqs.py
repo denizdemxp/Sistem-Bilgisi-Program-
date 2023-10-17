d92af33e1a3139974beaca63c1d8c6e8d517e689
XP808 <146320227+denizdemxp808@users.noreply.github.com>
2023-10-17 09:15:05 +0300

    Add files via upload

---
+++ reqs.py
@@ -0,0 +1,10 @@
+import platform
+
+print(platform.machine())
+print(platform.node())
+print(platform.platform)
+print(platform.processor())
+print(platform.release())
+print(platform.uname())
+
+# Bu Kod "https://allwin-raju-12.medium.com/get-system-information-using-python-26928d750ba2" Kaynağından Alınarak Yazılmıştır.
